a052ac11fa015c61dc05d8ac76122fd8283509d2
Rob <robg@mikucare.com>
2020-01-22 19:59:30 -0500

    saving progress
    Change-Id: Idd8c16a3ad1f56a4da80587850370ba29e4af9fa

--- drivers/spi/spidev.c
+++ drivers/spi/spidev.c
@@ -710,6 +710,7 @@ static const struct of_device_id spidev_dt_ids[] = {
 	{ .compatible = "rohm,dh2228fv", },
 	{ .compatible = "qcom,spi-msm-codec-slave", },
 	{ .compatible = "xethru,x4m03", },
+	{ .compatible = "linux,spidev" },
 	{},
 };
 
